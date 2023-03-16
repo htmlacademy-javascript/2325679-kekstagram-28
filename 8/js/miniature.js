c59966b085169589d9ba22a4fe52286c3cb68858
Keks <keks@htmlacademy.ru>
2023-03-16 22:34:02 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/miniature.js
@@ -0,0 +1,20 @@
+import {createPosts} from './data.js';
+
+const picturePlaceElement = document.querySelector('.pictures');
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
+const posts = createPosts();
+const postsFragment = document.createDocumentFragment();
+
+posts.forEach(({url, likes, comments}) => {
+  const pictureElement = pictureTemplate.cloneNode(true);
+  pictureElement.querySelector('.picture__img').src = url;
+  pictureElement.querySelector('.picture__likes').textContent = likes;
+  pictureElement.querySelector('.picture__comments').textContent = comments.length;
+
+  postsFragment.appendChild(pictureElement);
+});
+
+picturePlaceElement.appendChild(postsFragment);
+
+export {posts};
