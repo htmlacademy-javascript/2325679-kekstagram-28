1596f83e24e57300928aac8f8f2c0a31acb5719c
Keks <keks@htmlacademy.ru>
2023-03-16 20:07:51 +0300

    :heavy_check_mark: Сборка #7

--- 7/js/fullSizePicture.js
+++ 7/js/fullSizePicture.js
@@ -1,15 +1,18 @@
 import {posts} from './miniature.js';
 import {isEscapeKey} from './util.js';
 
-const pictures = document.querySelector('.pictures');
-const fullSizePicturePopup = document.querySelector('.big-picture');
-const fullSizePictureBlock = document.querySelector('.big-picture__img');
-const fullSizePicture = fullSizePictureBlock.querySelector('img');
-const likesCount = document.querySelector('.likes-count');
-const commentsCount = document.querySelector('.comments-count');
-const descriptionPicture = document.querySelector('.social__caption');
-const exitButton = document.querySelector('.big-picture__cancel');
-const commentsSocial = document.querySelector('.social__comments');
+const picturesElement = document.querySelector('.pictures');
+const fullSizePicturePopupElement = document.querySelector('.big-picture');
+const fullSizePictureBlockElement = document.querySelector('.big-picture__img');
+const fullSizePictureElement = fullSizePictureBlockElement.querySelector('img');
+const likesCountElement = document.querySelector('.likes-count');
+const commentsCountElement = document.querySelector('.comments-count');
+const descriptionPictureElement = document.querySelector('.social__caption');
+const exitButtonElement = document.querySelector('.big-picture__cancel');
+const commentsSocialElement = document.querySelector('.social__comments');
+
+const IMG_WIDTH = '35';
+const IMG_HEIGHT = '35';
 
 const onDocumentKeydown = (evt) => {
   if (isEscapeKey(evt)) {
@@ -20,35 +23,35 @@ const onDocumentKeydown = (evt) => {
 
 const showFullSizePictures = (evt) => {
   if (evt.target.matches('img[class="picture__img"]')) {
-    fullSizePicturePopup.classList.remove('hidden');
-    fullSizePicture.src = evt.target.src;
-    likesCount.textContent = evt.target.closest('.picture').querySelector('.picture__likes').textContent;
-    commentsCount.textContent = evt.target.closest('.picture').querySelector('.picture__comments').textContent;
+    fullSizePicturePopupElement.classList.remove('hidden');
+    fullSizePictureElement.src = evt.target.src;
+    likesCountElement.textContent = evt.target.closest('.picture').querySelector('.picture__likes').textContent;
+    commentsCountElement.textContent = evt.target.closest('.picture').querySelector('.picture__comments').textContent;
 
     document.querySelector('body').classList.add('modal-open');
     document.addEventListener('keydown', onDocumentKeydown);
     for (let i = 0; i < posts.length; i++) {
       if (evt.target.src.includes(posts[i].url)) {
-        descriptionPicture.textContent = posts[i].description;
-        commentsSocial.innerHTML = '';
+        descriptionPictureElement.textContent = posts[i].description;
+        commentsSocialElement.innerHTML = '';
         for (let j = 0; j < posts[i].comments.length; j++) {
 
-          const liElementPicture = document.createElement('li');
-          liElementPicture.classList.add('social__comment');
-          commentsSocial.appendChild(liElementPicture);
+          const liPictureElement = document.createElement('li');
+          liPictureElement.classList.add('social__comment');
+          commentsSocialElement.appendChild(liPictureElement);
 
-          const imgElementPicture = document.createElement('img');
-          imgElementPicture.classList.add('social__picture');
-          imgElementPicture.src = posts[i].comments[j].avatar;
-          imgElementPicture.alt = posts[i].comments[j].name;
-          imgElementPicture.width = '35';
-          imgElementPicture.height = '35';
-          liElementPicture.appendChild(imgElementPicture);
+          const imgPictureElement = document.createElement('img');
+          imgPictureElement.classList.add('social__picture');
+          imgPictureElement.src = posts[i].comments[j].avatar;
+          imgPictureElement.alt = posts[i].comments[j].name;
+          imgPictureElement.width = IMG_WIDTH;
+          imgPictureElement.height = IMG_HEIGHT;
+          liPictureElement.appendChild(imgPictureElement);
 
           const pElementPicture = document.createElement('p');
           pElementPicture.classList.add('social__text');
           pElementPicture.textContent = posts[i].comments[j].message;
-          liElementPicture.appendChild(pElementPicture);
+          liPictureElement.appendChild(pElementPicture);
         }
       }
     }
@@ -58,16 +61,16 @@ const showFullSizePictures = (evt) => {
 };
 
 
-const closeFullSizePictures = () => {
-  fullSizePicturePopup.classList.add('hidden');
+function closeFullSizePictures() {
+  fullSizePicturePopupElement.classList.add('hidden');
   document.querySelector('body').classList.remove('modal-open');
   document.removeEventListener('keydown', onDocumentKeydown);
-};
+}
 
-exitButton.addEventListener('click', () => {
+exitButtonElement.addEventListener('click', () => {
   closeFullSizePictures();
 });
 
-pictures.addEventListener('click', showFullSizePictures);
+picturesElement.addEventListener('click', showFullSizePictures);
 
 export {showFullSizePictures};
